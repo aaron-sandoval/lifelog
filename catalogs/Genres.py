5b43bf4ec29380e20415a3a75ad37d2309bdb4f1
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2024-04-09 11:26:16 -0600

    Refactored Genres.py with latest superclasses.

--- catalogs/Genres.py
+++ catalogs/Genres.py
@@ -1,32 +1,48 @@
 from yamlable import yaml_info, YamlAble
+from typing import Dict, Callable
 import kiwilib
 
-tag = 'Genre'
+
+@yaml_info(yaml_tag_ns='Genre')
+class Genre(kiwilib.HierarchicalEnum, kiwilib.Aliasable, YamlAble):
+    es_MX = 'Género'
+    color = (0.5, 0.5, 0.5)
+
+    def __init_subclass__(cls, **kwargs):
+        """Register all subclasses in the 'SocialGroup' namespace."""
+        super().__init_subclass__(**kwargs)
+        yaml_info(yaml_tag_ns='Genre')(cls)
+
+    @classmethod
+    def aliasFuncs(cls) -> Dict[str, Callable[['Genre'], str]]:
+        return {
+               # 'en_US': lambda slf: re.sub(r"([a-z])([A-Z])", r"\1 \2", type(slf).__name__),
+               'en_US': lambda slf: type(slf).__name__,
+               'es_MX': lambda slf: slf.es_MX if hasattr(slf, 'es_MX') else lambda slf: type(slf).__name__ + 'o'
+        }
+
+    @property
+    def color_hex(self) -> str:
+        """Hexadecimal string representation of the float color tuple."""
+        if isinstance(self.color[0], int):
+            return "#{0:02x}{1:02x}{2:02x}".format(*self.color)
+        else:
+            return "#{0:02x}{1:02x}{2:02x}".format(*[max(0, min(round(x*256), 255)) for x in self.color])
 
 
-@yaml_info(yaml_tag_ns=tag)
-class Genre(kiwilib.HierarchicalEnum, YamlAble): pass
-@yaml_info(yaml_tag_ns=tag)
-class GenreUndefined(Genre): pass  # Marks a Media instance as needing Genre(s) assigned. Used in DEFAULT_MEMBERS.
-@yaml_info(yaml_tag_ns=tag)
+class GenreUndefined(Genre):
+    # Marks a Media instance as needing Genre(s) assigned. Used in DEFAULT_MEMBERS.
+    es_MX = 'Indefinido'
+
+
 class Fiction(Genre): pass
-@yaml_info(yaml_tag_ns=tag)
 class NonFiction(Genre): pass
-@yaml_info(yaml_tag_ns=tag)
 class Comedy(Genre): pass
-@yaml_info(yaml_tag_ns=tag)
 class Mystery(Genre): pass
-@yaml_info(yaml_tag_ns=tag)
 class Historical(Genre): pass
-@yaml_info(yaml_tag_ns=tag)
 class Drama(Genre): pass
-@yaml_info(yaml_tag_ns=tag)
 class SciFi(Fiction): pass
-@yaml_info(yaml_tag_ns=tag)
 class Fantasy(Fiction): pass
-@yaml_info(yaml_tag_ns=tag)
 class Educational(NonFiction): pass
-@yaml_info(yaml_tag_ns=tag)
-class SelfHelp(Educational): pass
-@yaml_info(yaml_tag_ns=tag)
 class Opinion(NonFiction): pass
+class SelfHelp(Educational): pass
